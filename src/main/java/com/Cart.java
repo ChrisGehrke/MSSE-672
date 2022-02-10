ce89082c46ee55358adba64a96e070cac6fc5a13
Chris Gehrke <31970227+ChrisGehrke@users.noreply.github.com>
2022-02-10 15:32:44 -0700

    Add files via upload

---
+++ src/main/java/com/Cart.java
@@ -0,0 +1,23 @@
+//from techtutorial
+
+package com.model;
+
+public class Cart extends Houses{
+	private int quantity;
+	
+
+	
+
+	public Cart() {
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+}
