8bec90673cf351f675a4c6257d6c0d46a0c47509
Chris Gehrke <31970227+ChrisGehrke@users.noreply.github.com>
2022-03-09 20:54:43 -0700

    Add files via upload

---
+++ src/com/dao/HouseDao.java
@@ -0,0 +1,128 @@
+//from techtutorial
+
+package com.dao;
+
+import java.sql.*;
+import java.util.*;
+import com.model.Cart;
+import com.model.House;
+
+public class HouseDao {
+	private Connection con;
+
+	private String query;
+    private PreparedStatement pst;
+    private ResultSet rs;
+    
+
+	public HouseDao(Connection con) {
+		super();
+		this.con = con;
+	}
+	
+	
+	public List<House> getAllHouses() {
+        List<House> book = new ArrayList<>();
+        try {
+
+            query = "select * from houses";
+            pst = this.con.prepareStatement(query);
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+            	House row = new House();
+                row.setId(rs.getInt("id"));
+                row.setName(rs.getString("name"));
+                row.setCategory(rs.getString("category"));
+                row.setPrice(rs.getDouble("price"));
+                row.setImage(rs.getString("image"));
+
+                book.add(row);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return book;
+    }
+	
+	
+	 public House getSingleHouse(int id) {
+		 House row = null;
+	        try {
+	            query = "select * from houses where id=? ";
+
+	            pst = this.con.prepareStatement(query);
+	            pst.setInt(1, id);
+	            ResultSet rs = pst.executeQuery();
+
+	            while (rs.next()) {
+	            	row = new House();
+	                row.setId(rs.getInt("id"));
+	                row.setName(rs.getString("name"));
+	                row.setCategory(rs.getString("category"));
+	                row.setPrice(rs.getDouble("price"));
+	                row.setImage(rs.getString("image"));
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            System.out.println(e.getMessage());
+	        }
+
+	        return row;
+	    }
+	
+	public double getTotalCartPrice(ArrayList<Cart> cartList) {
+        double sum = 0;
+        try {
+            if (cartList.size() > 0) {
+                for (Cart item : cartList) {
+                    query = "select price from houses where id=?";
+                    pst = this.con.prepareStatement(query);
+                    pst.setInt(1, item.getId());
+                    rs = pst.executeQuery();
+                    while (rs.next()) {
+                        sum+=rs.getDouble("price")*item.getQuantity();
+                    }
+
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return sum;
+    }
+
+    
+    public List<Cart> getCartHouses(ArrayList<Cart> cartList) {
+        List<Cart> book = new ArrayList<>();
+        try {
+            if (cartList.size() > 0) {
+                for (Cart item : cartList) {
+                    query = "select * from houses where id=?";
+                    pst = this.con.prepareStatement(query);
+                    pst.setInt(1, item.getId());
+                    rs = pst.executeQuery();
+                    while (rs.next()) {
+                        Cart row = new Cart();
+                        row.setId(rs.getInt("id"));
+                        row.setName(rs.getString("name"));
+                        row.setCategory(rs.getString("category"));
+                        row.setPrice(rs.getDouble("price")*item.getQuantity());
+                        row.setQuantity(item.getQuantity());
+                        book.add(row);
+                    }
+
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return book;
+    }
+}
